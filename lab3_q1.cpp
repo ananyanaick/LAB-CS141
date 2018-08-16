ceaad0faebc29dffba6f10e022c84fa118c0f4ec
ananyanaick <42172605+ananyanaick@users.noreply.github.com>
2018-08-16 10:20:13 -0400

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,21 @@
+//operating with variables
+
+#include <iostream>
+using namespace std;
+int main()
+{
+//declaring vaariables;
+int a, b;
+int result;
+//process;
+a=5;
+b=2;
+a= a + 1;
+result = a - b;
+// print out result;
+cout << result;
+//terminate program;
+return 0;
+}
+
+
