f707062329da677aa650db4dd9b92d2d6ea2c692
ananyanaick <42172605+ananyanaick@users.noreply.github.com>
2018-08-16 11:25:29 -0400

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,23 @@
+//include library
+#include <iostream>
+using namespace std;
+//main function
+int main()
+{
+int a,b,sum,difference,product,remainder ; //declaration
+float divison;
+a = 5; //definition
+b = 3; //definition
+sum = a + b; //adds two numbers
+difference = a - b; // calculates the difference
+product = a * b; // calculates thr product
+remainder = a/b;
+//finds the remainder
+//displaying the calculations in the terminal
+cout << "numbers are " << a << "'" << b << endl;
+cout << "sum is" << sum << endl;
+cout << "difference is " << difference << endl;
+cout <<"product is" << product << endl;
+cout << "remainder is " << remainder << endl;
+return 0;
+}
