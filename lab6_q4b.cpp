04d17c5bff422d76ecb82e5ef8ce7e16e908d955
ananyanaick <42172605+ananyanaick@users.noreply.github.com>
2018-10-05 22:28:33 +0530

    Update lab6_q4b.cpp

--- lab6_q4b.cpp
+++ lab6_q4b.cpp
@@ -1,26 +1,27 @@
-//include libaries
+//including the library
 #include <iostream>
 using namespace std;
 //declaration of function
-void findmin(int a,int b,int &c)
-{//assign suitable value for c
-    if(a<b)
-        c=a;
-    else if(b<a)
-        c=b;
+void min(int a,int b,int &c)
+{
+//assigning a suitable value for variable c
+	if(a<b)
+		c=a;
+	else if(b<a)
+		c=b;
 }
-//main function
+//main function definition and declaration 
 int main()
-{    int a,b,c;
-    //ask the user for values for the varibles
-    cout<<"\nEnter any two values\n";
-    cin>>a>>b;
-    //call function
-    findmin(a,b,c);
-    //check equality and then print the min
-    if(c==0)
-        cout<<"\nBoth are equal\n";
-    else
-        cout<<"\nMinimum is "<<c<<endl;
-    return 0;
+{	int a,b,c;
+	//ask the user to enter the values for the variables
+	cout<<"\nEnter the two values to be compared\n”;
+	cin>>a>>b;
+	//calling the defined function min by reference
+	min(a,b,c);
+	//checking the equality and then printing the minimum value
+	if(c==0)
+		cout<<"\nBoth are equal\n";
+	else
+		cout<<"\nMinimum is "<<c<<endl;
+	return 0;
 }
