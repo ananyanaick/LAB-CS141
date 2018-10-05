7b72824a9179ee14ce8514db73b141dbe0d10801
ananyanaick <42172605+ananyanaick@users.noreply.github.com>
2018-10-05 21:57:14 +0530

    Update lab6_q2a.cpp

--- lab6_q2a.cpp
+++ lab6_q2a.cpp
@@ -1,20 +1,23 @@
 //include the library files and start the main
 #include<iostream>
 using namespace std;
-//sum function declaration and definition
-void sum(int a,int b){
-    //find sum
-    int s=a+b;
-    //print them
-    cout<<a<<"+"<<b<<"="<<s;
+//declaring and defining the sum function.
+void sum(int a,int b)
+{
+    //finding the sum
+    int sum=a+b;
+    //printing the sum in a+b=sum format
+    cout<<a<<"+"<<b<<"="<<sum;
     cout<<endl;
 }
-int main(){
+//end of function.
+int main()
+{
     
-    //declare varibles to which the numbers are stored
-    cout<<"Program to find sum of two functions using functions(call by value method)";
+    //declare variables to which the numbers are stored
+    cout<<"Program to find sum of two functions using functions";     //using the call by value method.
     int a,b;
-    cout<<"\nEnter any two numbers\n";
+    cout<<“\nEnter the two numbers to be added\n”;
     cin>>a>>b;
     
     //calling the function by value
