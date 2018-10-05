99d006158306cef320b31e70f1d5339fd6913af2
ananyanaick <42172605+ananyanaick@users.noreply.github.com>
2018-10-05 22:30:00 +0530

    Update lab6_q3b.cpp

--- lab6_q3b.cpp
+++ lab6_q3b.cpp
@@ -1,27 +1,27 @@
-//including library
+//including library 
 #include <iostream>
 using namespace std;
 //declaring and defining of the function
 void max(int a,int b,int &c)
 {
-    //putting value for c appropriately
-    if(a>b)
-        c=a;
-    else if(b>a)
-        c=b;
+//putting value for c appropriately 	
+	if(a>b)
+		c=a;
+	else if(b>a)
+		c=b;
 }
 //main function
 int main()
-{    int a,b,c;
-    //user enter values for the variables to be compared
-    cout<<"\nEnter the two numbers that are to be compared\n”;
-    cin>>a>>b;
-    //calling the function
-    max(a,b,c);
-    //checking for equality and then printing the maximum of the two
-    if(c==0)
-        cout<<"\nBoth entered numbers are equal";
-    else
-        cout<<"\nMaximum number of the two given is: "<<c;
-    return 0;
+{	int a,b,c;
+	//user enter values for the variables to be compared
+	cout<<"\nEnter the two numbers that are to be compared\n”;
+	cin>>a>>b;
+	//calling the function
+	max(a,b,c);
+	//checking for equality and then printing the maximum of the two
+	if(c==0)
+		cout<<"\nBoth entered numbers are equal";
+	else
+		cout<<"\nMaximum number of the two given is: "<<c;
+	return 0;
 }
